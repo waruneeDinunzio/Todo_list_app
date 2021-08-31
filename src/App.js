14d03b06596f9e0fce145ec211c74d17b5b5524e
Warunee Dinunzio <dinunziow@gmail.com>
2021-08-30 22:45:04 -0400

    create new react todo list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css'
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'dinner with kids',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+      <Todos todos={this.state.todos} />
+       
+      </div>
+    );
+  }
 }
 
 export default App;
